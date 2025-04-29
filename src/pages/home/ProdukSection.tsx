ece46950cb9022d5cfe84cf9c5b3e1fd05f63e0e
imdeni <im.deniachmad@gmail.com>
2025-04-29 10:33:46 +0700

    Update: Adding image and enchance view when product not found

--- src/pages/home/ProdukSection.tsx
+++ src/pages/home/ProdukSection.tsx
@@ -14,7 +14,7 @@ interface ProdukSectionProps {
 
 const ProdukSection: React.FC<ProdukSectionProps> = ({ filteredProducts }) => {
   return (
-    <section id="produk" className="bg-white p-10">
+    <section id="produk" className="bg-white p-10 scroll-mt-24">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 container mx-auto">
         {filteredProducts.map((product) => (
           <div key={product.id} className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-lg transition">
@@ -37,10 +37,19 @@ const ProdukSection: React.FC<ProdukSectionProps> = ({ filteredProducts }) => {
       </div>
 
       {filteredProducts.length === 0 && (
-        <div className="text-center text-gray-500 mt-10">
+      <div className="flex flex-col items-center justify-center w-full mt-10 text-center text-gray-500">
+        <img 
+          src="/notfound.jpg"
+          alt="Produk tidak ditemukan"
+          className="w-64 h-64 object-contain mb-6"
+        />
+        <p className="text-lg font-semibold mb-4">
           Produk tidak ditemukan.
-        </div>
-      )}
+        </p>
+      </div>
+    )}
+
+
     </section>
   );
 };
